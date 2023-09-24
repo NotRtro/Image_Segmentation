2cd81b70012908a4ddfcf087a426651a0f9f1161
ronaldoFlores3552 <ronaldo.flores@utec.edu.pe>
2023-09-23 20:41:35 -0500

    Agregando Vue al Front

---
+++ VueFrontend/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="container">
+    <div>
+      <h1>BrandVista<span>Chamo++</span></h1>
+      <div class="link">
+        <ul>
+          <li><a href="#" >Caracteristicas</a></li>
+          <li><a href="#">Acerca de</a></li>
+          <li><a href="#">Testimonios</a></li>
+          <li><a href="#">Contactanos</a></li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <H1>Segmentación de imágenes</H1>
+      <p>Somos un servicio que brinda un plan de Marketing a pequeñas y medianas empresas ayudando a conocer
+        datos estadisticos de los clientes a partir del análisis de imágenes.</p>
+        <button type="button" class="btn btn-primary">WATCH VIDEO</button>
+        <button type="button" class="btn btn-default" >GET STARTED</button>
+    </div>
+
+
+  </header>
+</template>
+
