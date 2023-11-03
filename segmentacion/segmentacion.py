2183f11ec85f0093ab52634e1c6dae9c8a593a2b
Sael-init <83047305+Sael-init@users.noreply.github.com>
2023-11-03 16:56:45 -0500

    Add files via upload

--- segmentacion/segmentacion.py
+++ segmentacion/segmentacion.py
@@ -23,6 +23,7 @@ print("blob.shape:",blob.shape)
 
 net.setInput(blob)
 detections = net.forward()
+i=0
 for detection in detections[0][0]:
     print(detection[1])
     if  detection[2]>0.45:
@@ -34,7 +35,12 @@ for detection in detections[0][0]:
         cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
         cv2.putText(image,"Confg:{:.2f}".format(detection[2]*100), (x_start, y_start-5),1,1.2,(255,0,0),2)
         cv2.putText(image,label,(x_start, y_start - 25),1,1.2,(255,0,0,0),2)
-
+        
+        roi = image[y_start:y_end, x_start:x_end]
+        cv2.imwrite("./temporal/"+str(i)+".jpg", roi)
+        i+=1
+        
 cv2.imshow("image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
