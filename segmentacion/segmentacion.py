85807370dc832509e0b8b980f2d260fd392e21d1
Sael-init <83047305+Sael-init@users.noreply.github.com>
2023-11-03 19:27:12 -0500

    Add files via upload

--- segmentacion/segmentacion.py
+++ segmentacion/segmentacion.py
@@ -12,7 +12,7 @@ classes = { 0: 'background',
     17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor' }
 
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
-image = cv2.imread("img2.png")
+image = cv2.imread("car4.jpg")
 
 height, width, _ = image.shape
 image_resized =cv2.resize(image, (300, 300))
@@ -23,22 +23,31 @@ print("blob.shape:",blob.shape)
 
 net.setInput(blob)
 detections = net.forward()
+
+print("Seleccione las clases que desea procesar separadas por espacios:")
+for key, value in classes.items():
+    print(f"{key}: {value}")
+
+selected_classes = input("Clases seleccionadas (ej. 1 2 3): ").split()
+selected_classes = [int(class_id) for class_id in selected_classes if class_id.isdigit() and int(class_id) in classes]
+print(selected_classes)
+
 i=0
 for detection in detections[0][0]:
-    print(detection[1])
+    
     if  detection[2]>0.45:
         label_index = int(detection[1])
-        label =classes[label_index]
-        print("Label" + label)
-        box = detection[3:7] * [width, height, width, height]
-        x_start, y_start, x_end, y_end = int(box[0]), int(box[1]), int(box[2]), int(box[3])
-        cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
-        cv2.putText(image,"Confg:{:.2f}".format(detection[2]*100), (x_start, y_start-5),1,1.2,(255,0,0),2)
-        cv2.putText(image,label,(x_start, y_start - 25),1,1.2,(255,0,0,0),2)
-        
-        roi = image[y_start:y_end, x_start:x_end]
-        cv2.imwrite("./temporal/"+str(i)+".jpg", roi)
-        i+=1
+        if label_index in selected_classes:
+            label =classes[label_index]
+            box = detection[3:7] * [width, height, width, height]
+            x_start, y_start, x_end, y_end = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+            cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
+            cv2.putText(image,"Confg:{:.2f}".format(detection[2]*100), (x_start, y_start-5),1,1.2,(255,0,0),2)
+            cv2.putText(image,label,(x_start, y_start - 25),1,1.2,(255,0,0,0),2)
+            
+            roi = image[y_start:y_end, x_start:x_end]
+            cv2.imwrite("./temporal/"+str(i)+".jpg", roi)
+            i+=1
         
 cv2.imshow("image", image)
 cv2.waitKey(0)
